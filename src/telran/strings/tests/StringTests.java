0d5d5ac295f498c7ddade88216851d9ccc5bd331
allan <allanteone@gmail.com>
2023-05-26 23:54:00 +0300

    hw-1

---
+++ src/telran/strings/tests/StringTests.java
@@ -0,0 +1,44 @@
+package telran.strings.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTests {
+
+	@Test
+	void testEqualsIgnoreCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareToIgnoreCase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStartsWithString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEndsWith() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIndexOfString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConcat() {
+		fail("Not yet implemented");
+	}
+
+}
