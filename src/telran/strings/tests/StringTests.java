8c6f86e1ce9587508d00c58fff750b9e93e6b8aa
allan <allanteone@gmail.com>
2023-05-28 08:17:50 +0300

    hw-1

--- src/telran/strings/tests/StringTests.java
+++ src/telran/strings/tests/StringTests.java
@@ -8,7 +8,14 @@ class StringTests {
 
 	@Test
 	void testEqualsIgnoreCase() {
-		fail("Not yet implemented");
+		String str = "abcd";
+		
+		assertTrue(str.equalsIgnoreCase(str));
+		assertTrue(str.equalsIgnoreCase("ABCD"));
+		
+		assertFalse(str.equalsIgnoreCase("abc"));
+		assertFalse(str.equalsIgnoreCase("abcde"));
+		assertFalse(str.equalsIgnoreCase("bcd"));
 	}
 
 	@Test
@@ -29,27 +36,89 @@ class StringTests {
 
 	@Test
 	void testCompareToIgnoreCase() {
-		fail("Not yet implemented");
+		String str = "abcd";
+		
+		assertEquals(0, str.compareToIgnoreCase("Abcd"));
+		assertEquals(0, str.compareToIgnoreCase("aBcd"));
+		assertEquals(0, str.compareToIgnoreCase("abCd"));
+		assertEquals(0, str.compareToIgnoreCase("abcD"));
+		
+		assertTrue(str.compareToIgnoreCase("Abcdefg") < 0);
 	}
 
 	@Test
-	void testStartsWithString() {
-		fail("Not yet implemented");
+	void testStartsWith() {
+		String str = "abcd";
+		
+		assertTrue(str.startsWith(str));
+		assertTrue(str.startsWith("abc"));
+		assertTrue(str.startsWith("ab"));
+		assertTrue(str.startsWith("ab"));
+		
+		assertFalse(str.startsWith("bcd"));
+		assertFalse(str.startsWith("cd"));
+		assertFalse(str.startsWith("d"));
+		
+		assertFalse(str.startsWith("Abcd"));
+		
 	}
 
 	@Test
 	void testEndsWith() {
-		fail("Not yet implemented");
+		String str = "abcd";
+		
+		assertTrue(str.endsWith(str));
+		assertTrue(str.endsWith("bcd"));
+		assertTrue(str.endsWith("cd"));
+		assertTrue(str.endsWith("d"));
+		
+		assertFalse(str.endsWith("abc"));
+		assertFalse(str.endsWith("ab"));
+		assertFalse(str.endsWith("a"));
+		
+		assertFalse(str.endsWith("abcD"));
+		
 	}
 
 	@Test
-	void testIndexOfString() {
-		fail("Not yet implemented");
+	void testIndexOf() {
+		String str = "Hello planet earth, you are a great planet.";
+		
+		assertEquals(0, str.indexOf(str));
+		
+		assertEquals(0, str.indexOf("Hello"));
+		assertEquals(5, str.indexOf(" "));
+		assertEquals(str.length() - 1, str.indexOf("."));
+		
+		assertEquals(-1, str.indexOf("hello"));
+		assertEquals(-1, str.indexOf("1"));
+		assertEquals(-1, str.indexOf("  "));
+	}
+	
+	@Test
+	void testLastIndexOf() {
+		String str = "Hello planet earth. You are a great planet.";
+				
+		assertEquals(0, str.lastIndexOf(str));
+		
+		assertEquals(str.length() - 1, str.lastIndexOf("."));
+		assertEquals(str.length() - 8, str.lastIndexOf(" "));
+		
+		assertEquals(-1, str.lastIndexOf("hello"));
+		assertEquals(-1, str.lastIndexOf("1"));
+		assertEquals(-1, str.lastIndexOf("  "));
+		
+		
 	}
 
 	@Test
 	void testConcat() {
-		fail("Not yet implemented");
+		String str = "abcd";
+		
+		assertTrue(str.concat(str).equals(str + str));
+		
+		assertTrue(str.concat("1").equals(str + "1"));
+		assertTrue("1".concat(str).equals("1" + str));
 	}
 
 }
